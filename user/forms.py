70086a6746524f004ce9fc699ef7533cac27af97
Sanjith Kumar <53137451+sanjith-kumar-048@users.noreply.github.com>
2021-06-26 16:06:54 +0000

    Update forms.py

--- user/forms.py
+++ user/forms.py
@@ -11,3 +11,5 @@ class UserCreateForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields["username"].label = "Display name"
         self.fields["email"].label = "Email address"
+
+        
